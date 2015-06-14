02840d118c6ae7d0df050bff5f7fe203cf88ba2e
kilbergr <rekilberg@gmail.com>
2015-06-13 18:32:48 -0700

    with randomsong

--- app.js
+++ app.js
@@ -12,6 +12,27 @@ app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended:true}));
 
+//choose a random song
+var songIds = ["995535015", "966411602", "823593456", "956689796", "943946671",
+                 "982388023", "907242704", "201281527", "656801339", "910038357",
+                 "250038575", "878000348",  "794095205",  "1645339",  "400835962",
+                 "325618", "191924084",  "376116617",  "169003415",  "51958108",
+                 "76532142", "192688540", "684811768", "344799464", "217633921",
+                 "192811017", "258404365", "71068886", "640047583", "517438248" ];
+
+//choose random element
+var playGuess = function(){
+	return Math.floor(Math.random()*songIds.length);
+	};
+// var play = function(song){
+// 	var audio = document.getElementById("audio_id");
+// 	audio.src="song.previewURL";
+// 	audio.oncanplay = function() {
+// 		console.log("audio can play");
+//   	audio.play();
+//    }
+// }
+
 //root
 app.get("/", function(req, res){
 	res.redirect("/scores")
@@ -46,16 +67,22 @@ app.post("/scores", function(req, res){
 });
 
 
+
 //randomsong
-app.get("/scores/randomsong", function(req, res){
-	var url = "https://itunes.apple.com/lookup?id=" + "376116617";
+app.get("/randomsong", function(req, res){
+	var randSong = playGuess();
+	var url = "https://itunes.apple.com/lookup?id=" + songIds[randSong];
 	request(url, function(error, response, body){
 		if(error){
 			res.render('errors/500')
 		}
-		else if(!error && response.status===200){
-			songs = JSON.parse(body);
+		else if(!error && response.statusCode ===200){
+			song = JSON.parse(body).results[0];
+			//play(song);
 			res.render('scores/randomsong');
+		}
+		else{
+			res.send(response.statusCode + "");
 		}
 	})
 })
@@ -68,6 +95,7 @@ app.get('/scores/:id', function(req, res){
 		}
 		else{
 			res.render('scores/show', {score:score});
+			//res.json(score);
 		}
 	})
 });
@@ -86,12 +114,12 @@ app.get("/scores/:id/edit", function(req, res){
 
 //update
 app.put("/scores/:id", function(req, res){
-	db.Score.findByIdAndUpdate(req.params.id, req.body.score, function(err, scores){
+	db.Score.findByIdAndUpdate(req.params.id, req.body.score, function(err){
 		if(err){
 			res.render('errors/500')
 		}
 		else{
-			res.redirect('/scores', {scores: scores})
+			res.redirect('/scores')
 		}
 	})
 });
@@ -108,9 +136,6 @@ app.delete("/scores/:id", function(req, res){
 	})
 });
 
-
-
-
 app.listen(3000, function() {
-  console.log("You started the serever on port 3000, well done!");
+  console.log("You started the server on port 3000, well done!");
 });
--- public/javascript/songs.js
+++ public/javascript/songs.js
@@ -1,30 +1,14 @@
 $(function() {
-var wins = 0;
-var loses = 0;
-  var songIds = ["995535015", "966411602", "823593456", "956689796", "943946671", "900672609",
-                 "982388023", "907242704", "201281527", "684545030", "656801339", "910038357", "499875050",
-                 "250038575", "878000348", "493341471", "794095205", "189219134", "192678292",  "1645339", "400835962",
-                 "325618", "191924084",  "376116617", "401164378", "169003415",  "51958108", "703590286",
-                 "76532142", "192688540", "684811768", "540020920", "344799464", "217633921",
-                 "192811017", "907242703", "258404365", "71068886", "640047583", "517438248" ];
+	var wins = 0;
+	var loses = 0;
 
-var playGuess = function(){
-	//choose random element
-	var randSong = Math.floor(Math.random()*songIds.length);
-	var data;
-	//ajax lookup by random song ID
-		$.ajax({
-		   url: "https://itunes.apple.com/lookup?id=" + songIds[randSong],
-		   jsonp: "callback",
-		   dataType: "jsonp"
-		   //function to get song from the looked up id
-		}).done(function(songInfo) {
+	var play = function(songInfo) {
 			//play song
 			data = songInfo.results[0];
-				$("#audio_id").attr("src", data.previewUrl);
-				$("#audio_id").on("canplay", function() {
-  			$("#audio_id")[0].play();
-				});
+			$("#audio_id").attr("src", data.previewUrl);
+			$("#audio_id").on("canplay", function() {
+			$("#audio_id")[0].play();
+			});
 
 			//enter guess and check match song of to guess
 			$("input").off("keypress");
@@ -32,19 +16,13 @@ var playGuess = function(){
 				if(e.which === 13){
 					e.preventDefault();
 					var guess = this.value;
-						if(guess===data.trackName || guess === data.artistName || guess === data.collectionName || guess=== data.collectionCensoredName || guess===data.trackCensoredName){
-							wins++;
-	     					});
-						}
-						else {
-							loses++;
-						}
-					}
-				});
-		});	
-	}
-	playGuess();
-		$("button").on("click", function(){
-			playGuess();
-		})
+					if(guess===data.trackName || guess === data.artistName || guess === data.collectionName || guess=== data.collectionCensoredName || guess===data.trackCensoredName){
+						wins++;
+					};
+				}
+				else {
+					loses++;
+				}
+			})
+		};
 });
