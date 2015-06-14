6d71d54bd93164d7fd48a52d7b403bf8f21e8464
kilbergr <rekilberg@gmail.com>
2015-06-14 12:24:38 -0700

    working matching game

--- public/javascript/songs.js
+++ public/javascript/songs.js
@@ -8,24 +8,49 @@ $(function() {
 		}
 
 	var guess = function(){
-		$("#guess").on("click", function(event){
+		$("#songID").on("submit", function(event){
 			event.preventDefault();
-			var guess = $("input");
-			var album = new RegExp($("#albumName")[0].innerHTML);
-			var artist = new RegExp($("#artistName")[0].innerHTML);
-			var track = new RegExp($("#songName")[0].innerHTML);
-			console.dir(guess);
-			//if(guess.search(track) !== -1);{
-		//		console.log("yes");
-			//}
+			var guess = $("input.songinput")[0].value;
+			var album = $("#albumName")[0].innerHTML;
+			var artist = $("#artistName")[0].innerHTML;
+			var track = $("#songName")[0].innerHTML;
+
+			if(guess === album || guess === artist  || guess === track){
+				wins++;
+				$("#correct").css("visibility", "visible");
+				$("#win").css("display", "inline");
+				$("#incorrect").css("visibility", "hidden");
+				$("#loss").css("display", "none");
+				$("#board").css({
+					"background-image": "url(http://media.giphy.com/media/10Y3CxSnTI2kFO/giphy.gif)", 
+					"background-attachment": "fixed",
+					"background-size": "30%"
+				});
+				$("th").css("color", "white");
+				$("h2").css("color", "white");
+				$("td").css("color", "white");
+			}
+			else {
+				losses++;
+				$("#incorrect").css("visibility", "visible");
+				$("#loss").css("display", "inline");
+				$("#correct").css("visibility", "hidden");
+				$("#win").css("display", "none");
+				$("#board").css({
+					"background-image": "url(http://rogerbourland.com/wp-content/uploads/2012/08/arthurschatz1.jpg)", 
+					"background-attachment": "fixed",
+					"background-size": "30%" 
+				});		
+				$("th").css("color", "white");
+				$("h2").css("color", "white");		
+				$("td").css("color", "white");	
+			}
+
+			$("#wins").html(wins);
+			$("#losses").html(losses);
+		//	$("#songSolution").html("<b>Song</b>: " + song.trackName + ", <b>Artist</b>: " + song.artistName + ", <b>Album</b>: " + song.collectionName);
+			$("#solution").css("visibility", "visible");
 			
-			// if(guess === album || guess === artist  || guess === track){
-			// 	wins++;
-			// }
-			// else {
-			// 	losses++;
-			// }
-			// console.log("wins: " + wins+ "; losses: " + losses);
 		})
 	}
 		playIt();
